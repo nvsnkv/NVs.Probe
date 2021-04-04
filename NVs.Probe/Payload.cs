c7e18791b7a524fdcbc046a8740cc54ee2d60674
Nikita Senkov <nvsnkv@live.com>
2021-04-04 15:15:24 +0300

    app skeleton

---
+++ NVs.Probe/Payload.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+
+namespace NVs.Probe
+{
+    class Payload : IHostedService
+    {
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- NVs.Probe/Program.cs
+++ NVs.Probe/Program.cs
@@ -1,12 +1,40 @@
 ﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
 
 namespace NVs.Probe
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            throw new NotImplementedException("It's far away from MVP so far!");
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateLogger();
+            try
+            {
+                Log.Information("Starting host..");
+                BuildHost(args).Run();
+                return 0;
+            }
+            catch (Exception e)
+            {
+                Log.Fatal(e, "Host terminated unexpectedly!");
+                return -1;
+            }
+        }
+
+        private static IHost BuildHost(string[] args)
+        {
+            return new HostBuilder()
+                .ConfigureServices(s => s.AddSingleton<IHostedService>(new Payload()))
+                .UseSerilog()
+                .Build();
         }
     }
 }
